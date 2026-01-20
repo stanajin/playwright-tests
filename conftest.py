fbefb034ab363d274cb5d7de07231a0d5002d049
Sachin Nimbalkar <nimbalkarsachin@outlook.com>
2026-01-20 19:01:27 +0000

    Add config to use custom viewport size

--- conftest.py
+++ conftest.py
@@ -13,3 +13,14 @@ def load_env():
     base_url = os.getenv("BASE_URL")
     if base_url:
         os.environ["base_url"] = base_url
+
+
+@pytest.fixture(scope="session")
+def browser_context_args(browser_context_args):
+    return {
+        **browser_context_args,
+        "viewport": {
+            "width": 1920,
+            "height": 1080,
+        }
+    }
